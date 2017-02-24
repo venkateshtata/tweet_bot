f012ea25172a943bbc76a81c03452ae48cbefc84
venkatesh <venkateshtata9@gmail.com>
2017-02-24 20:07:54 +0530

    quiries the twitter with user entered keyword

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'bTX7mdTx3IDpbOTFKF7PmGTBJ',
+  consumer_secret:      'vOl8EACJRxb3sc8kvlqAZYIgsSlqyOMXLJQmTZT1ccHKvbxKKS',
+  access_token:         '803421557901012992-KT8zkgl16VdVkfCH8OwxW26GUUeAO4O',
+  access_token_secret:  'tavghUDwC2ZIgHPgzQvOd9qPy1s6yM7JULIRReGH2cQ7b'
+}
